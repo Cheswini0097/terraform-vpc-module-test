34c95afadfd3d294d31ce5b9a536dba38d2173a0
chethan <cheswinreddy@gmail.com>
2024-12-10 18:31:57 +0530

    terraform

---
+++ test-vpc.tf
@@ -0,0 +1,13 @@
+module "vpc" {
+    source = "../terraform-vpc-module"
+    project_name = "expense"
+    envinorment = "dev"
+    common_tags = var.common_tags
+    public_subnet_cidrs = var.public_subnet_cidrs
+    private_subnet_cidrs = var.private_subnet_cidrs
+    database_subnet_cidrs = var.database_subnet_cidrs
+    is_peering_required = var.is_peering_required
+    
+
+
+}
